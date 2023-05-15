cdd46d0a9a56cfd0d6c3367da19fe092298bbcb5
Moonquakeyu <101801812+Moonquakeyu@users.noreply.github.com>
2023-05-15 12:45:21 +0800

    modified:   src/Module/Cards/ActionCard.java

--- src/Module/Cards/ActionCard.java
+++ src/Module/Cards/ActionCard.java
@@ -1,42 +1,57 @@
 package Module.Cards;
 
 import java.util.ArrayList;
+//这里需要加上卡牌的颜色
 
-public class ActionCard extends Card {
-    public ActionCard(String name) {
-        super(name);
+
+import javax.swing.*;
+
+public abstract class ActionCard extends Card {
+    public ActionCard(String name, ImageIcon cardImage) {
+        super(name, cardImage);
+    }
+
+    @Override
+    public void play() {
+        // TODO: 实现 play 方法
+    }
+
+    @Override
+    public void putBack() {
+        // TODO: 实现 putBack 方法
     }
 
 
-    public static ArrayList<Card> initializeCardsForCardsPile() {
-        ArrayList<Card> actionCards = new ArrayList<>();
 
-        actionCards.add(new ActionCard("Double The Rent Cards"));
-        actionCards.add(new ActionCard("Double The Rent Cards"));
-        actionCards.add(new ActionCard("Deal Breaker"));
-        actionCards.add(new ActionCard("Deal Breaker"));
+    // public static ArrayList<Card> initializeCardsForCardsPile() {
+    //     ArrayList<Card> actionCards = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("Just Say No"));
-        }
-        for (int i = 0; i < 4; i++) {
-            actionCards.add(new ActionCard("Force Deal"));
-        }
-        for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("Debt Collector"));
-        }
-        for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("It’s My Birthday"));
-        }
-        for (int i = 0; i < 10; i++) {
-            actionCards.add(new ActionCard("Pass Go"));
-        }
-        for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("House"));
-        }
-        for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("Hotel"));
-        }
-        return actionCards;
-    }
+    //     actionCards.add(new ActionCard("Double The Rent Cards"));
+    //     actionCards.add(new ActionCard("Double The Rent Cards"));
+    //     actionCards.add(new ActionCard("Deal Breaker"));
+    //     actionCards.add(new ActionCard("Deal Breaker"));
+
+    //     for (int i = 0; i < 3; i++) {
+    //         actionCards.add(new ActionCard("Just Say No"));
+    //     }
+    //     for (int i = 0; i < 4; i++) {
+    //         actionCards.add(new ActionCard("Force Deal"));
+    //     }
+    //     for (int i = 0; i < 3; i++) {
+    //         actionCards.add(new ActionCard("Debt Collector"));
+    //     }
+    //     for (int i = 0; i < 3; i++) {
+    //         actionCards.add(new ActionCard("It’s My Birthday"));
+    //     }
+    //     for (int i = 0; i < 10; i++) {
+    //         actionCards.add(new ActionCard("Pass Go"));
+    //     }
+    //     for (int i = 0; i < 3; i++) {
+    //         actionCards.add(new ActionCard("House"));
+    //     }
+    //     for (int i = 0; i < 3; i++) {
+    //         actionCards.add(new ActionCard("Hotel"));
+    //     }
+    //     return actionCards;
+    // }
 }
