efbdd91afacd3336cc0475fa0b91697f46a191ed
Moonquakeyu <101801812+Moonquakeyu@users.noreply.github.com>
2023-05-15 19:39:36 +0800

    modified:   src/Module/Cards/ActionCard.java 	modified:   src/Module/Cards/MoneyCard.java 	modified:   src/Module/Cards/PropertyCard.java 	modified:   src/Module/Cards/RentCard.java

--- src/Module/Cards/MoneyCard.java
+++ src/Module/Cards/MoneyCard.java
@@ -2,6 +2,8 @@ package Module.Cards;
 
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+
 public class MoneyCard extends Card implements CardImplement {
     private int value;
 
@@ -27,22 +29,22 @@ public class MoneyCard extends Card implements CardImplement {
     public static ArrayList<Card> initializeCardsForCardsPile() {
         ArrayList<Card> moneyCards = new ArrayList<>();
         for (int i = 0; i < 6; i++) { // 初始化6张1$MoneyCard
-            moneyCards.add(new MoneyCard("1M"));
+            moneyCards.add(new MoneyCard("1M",null,1));
         }
         for (int i = 0; i < 5; i++) { // 初始化5张2$MoneyCard
-            moneyCards.add(new MoneyCard("2M"));
+            moneyCards.add(new MoneyCard("2M",null,2));
         }
         for (int i = 0; i < 4; i++) { // 初始化4张3$MoneyCard
-            moneyCards.add(new MoneyCard("3M"));
+            moneyCards.add(new MoneyCard("3M",null,3));
         }
         for (int i = 0; i < 3; i++) { // 初始化3张4$MoneyCard
-            moneyCards.add(new MoneyCard("4M"));
+            moneyCards.add(new MoneyCard("4M",null,4));
         }
         for (int i = 0; i < 2; i++) { // 初始化2张5$MoneyCard
-            moneyCards.add(new MoneyCard("5M"));
+            moneyCards.add(new MoneyCard("5M",null,5));
         }
         for (int i = 0; i < 1; i++) { // 初始化1张10$MoneyCard
-            moneyCards.add(new MoneyCard("10M"));
+            moneyCards.add(new MoneyCard("10M",null,10));
         }
         return moneyCards;
     }
