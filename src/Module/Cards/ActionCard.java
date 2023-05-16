8b4becfe42c0b29a501a608ad27da2c00284bb2c
Moonquakeyu <101801812+Moonquakeyu@users.noreply.github.com>
2023-05-16 15:40:12 +0800

    modified:   images/.DS_Store 	modified:   src/Module/Cards/ActionCard.java 	modified:   src/Module/Cards/PropertyCard.java

--- src/Module/Cards/ActionCard.java
+++ src/Module/Cards/ActionCard.java
@@ -3,40 +3,51 @@ package Module.Cards;
 import java.awt.*;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 public class ActionCard extends Card {
-    public ActionCard(String name, Image image) {
+    public ActionCard(String name, ImageIcon image) {
         super(name,image);
     }
 
 
     public static ArrayList<Card> initializeCardsForCardsPile() {
         ArrayList<Card> actionCards = new ArrayList<>();
-
-        actionCards.add(new ActionCard("Double The Rent Cards",null));
-        actionCards.add(new ActionCard("Double The Rent Cards",null));
-        actionCards.add(new ActionCard("Deal Breaker",null));
-        actionCards.add(new ActionCard("Deal Breaker",null));
+        ImageIcon doubleIcon = new ImageIcon("images/Card/ADouble.pic.jpg");
+        ImageIcon dealIcon = new ImageIcon("images/Card/ADeal.pic.jpg");
+        actionCards.add(new ActionCard("Double The Rent Cards",doubleIcon));
+        actionCards.add(new ActionCard("Double The Rent Cards",doubleIcon));
+        actionCards.add(new ActionCard("Deal Breaker",dealIcon));
+        actionCards.add(new ActionCard("Deal Breaker",dealIcon));
+        
 
         for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("Just Say No",null));
+            var justIcon = new ImageIcon("images/Card/AJustno.pic.jpg");
+            actionCards.add(new ActionCard("Just Say No",justIcon));  
         }
         for (int i = 0; i < 4; i++) {
+            var icon= new ImageIcon("images/Card/AForce.pic.jpg");
             actionCards.add(new ActionCard("Force Deal",null));
         }
         for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("Debt Collector",null));
+            var icon= new ImageIcon("images/Card/ADebt.pic.jpg");
+            actionCards.add(new ActionCard("Debt Collector",icon));
         }
         for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("It’s My Birthday",null));
+            var icon= new ImageIcon("images/Card/ABirthday.pic.jpg");
+            actionCards.add(new ActionCard("It's My Birthday",icon));
         }
         for (int i = 0; i < 10; i++) {
-            actionCards.add(new ActionCard("Pass Go",null));
+            var icon= new ImageIcon("images/Card/APass.pic.jpg");
+            actionCards.add(new ActionCard("Pass Go",icon));
         }
         for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("House",null));
+            var icon= new ImageIcon("images/Card/AHouse.pic.jpg");
+            actionCards.add(new ActionCard("House",icon));
         }
         for (int i = 0; i < 3; i++) {
-            actionCards.add(new ActionCard("Hotel",null));
+            var icon= new ImageIcon("images/Card/AHotel.pic.jpg");
+            actionCards.add(new ActionCard("Hotel",icon));
         }
         return actionCards;
     }
