25b6a575e9fccfaa8974df0a5c6c783023bd2c50
LiYanTao <641217688tly@gmail.com>
2023-05-16 10:01:59 +0800

    null

--- src/Module/PlayerAndComponents/Bank.java
+++ src/Module/PlayerAndComponents/Bank.java
@@ -1,10 +1,3 @@
-package Module.PlayerAndComponents;
-
-import Module.Cards.Card;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
 /*
 Bank类将主要为玩家提供服务(每个玩家都有自己的Bank),它应该具有如下属性和功能:
 属性:
@@ -19,6 +12,13 @@ Bank类将主要为玩家提供服务(每个玩家都有自己的Bank),它应该
     (因此需要实现)统计总存款
 自动交租金(实现某种算法,使得玩家在被要求收钱后如果长时间不行动,就自动从银行中以最优的方式扣款)
 */
+package Module.PlayerAndComponents;
+
+import Module.Cards.Card;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Bank extends JDialog {
     public static ArrayList<Card> moneyCards;
 
